f568a463a7053fca8203c5a2ceb8b1a22205a500
XavierG24 <zavexvx@gmail.com>
2018-09-06 23:18:33 -0700

    timer done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,4 @@
-
-
-// $("#start").on("click", function(){
-//     //function that displays Q's + timer and starts timer 
-//     var timer = setTimeout(function(){
-//     }, 60000);
-
-//     setInterval(function() {
-//         number--, 1000
-//     });
-//     clearInterval(intervalID);
-//     // $(".questions").html("<p>" + questions + "</p>");
-
-
-// });
-
-// var number = 60;
-// var intervalID;
-// var questions = ["What year was Back to the Future released?", 
-// "In what city did Ferris Bueller take his day off?"];
-
-// $("#timer").html("<p>Time Remaining: " + number + "</p>");
-
-// setInterval(function () {
-//     alert("Time's Up!")
-// }, 60000);
-
-
-// function run() {
-//     clearInterval(intervalId);
-//     intervalId = setInterval(decrement, 1000);
-// };
-
-// function decrement() {
-//     var number = 60;
-//     number--;
-//     clearInterval(intervalID);
-//     $("#timer").html("<p>Time Remaining: " + number + "</p>");
-
-
-//     if (number === 0) {
-//         stop();
-//         alert("Time's up!");
-//     }
-
-// };
-
-// function stop() {
-//     clearInterval(intervalID);
-// }
-
-
-// run();
-
 //pseudocode
-
 //user opens page, title shows and START BUTTON is there
 //user CLICKS start
 //after clicking start TIMER and QUESTIONS DISPLAY on screen
@@ -61,24 +6,63 @@
 //
 
 var number = 60;
+var intervalID;
+var correctAnswer;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
 
+
+$(".questions").hide();
+$(".timer").hide();
+$("#results").hide();
+
+//when start is clicked, questions appear, timer appears and is counting down...
 $(document).ready(function () {
 
-    $(".questions").hide();
-    $(".timer").hide();
-
     $("#start").on("click", function () {
-        setTimeout(function(){
-            decrement, 1000
-        })
-        $("#timer").html(number); 
+
         $(".questions").show();
         $(".timer").show();
 
-    })
+    });
+
+    function run() {
+        intervalId = setInterval(decrement, 1000);
+    };
 
 
+    function decrement() {
+        $(".timer").html("<h2>Time Remaining: " + number + "</h2>");
+        number--;
 
 
+        if (number === -1) {
+            stop();
+            $(".questions").hide();
+            $("#results").show();
+        };
 
+
+    };
+
+   $(".correct").on("click", function () {
+        correctAnswers++;
+        console.log(correctAnswers);
+        //display score
+        $("#right-answers").text(correctAnswers);
+    });
+
+    //check if guess was right
+    //if guessed right show on screen
+    // else increment incorrect guesses by 1
+
+    
+
+    function stop() {
+        clearInterval(intervalId);
+       
+    };
+
+    run();
 });
